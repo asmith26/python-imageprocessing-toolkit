46f1c22c7ea6896d502ad2302431ec5b11d13050
Andrew Smith <asmith26@users.noreply.github.com>
2016-05-15 17:38:30 +0100

    Initial commit.

--- imageproc_tk/common.py
+++ imageproc_tk/common.py
@@ -19,3 +19,24 @@ def img2arr(img_path, set_height=0, set_width=0,  scale_width=1, scale_height=1,
                          dsize=(set_width, set_height),
                          fx=scale_width, fy=scale_height)
     return resized
+
+
+def height_width_channels(arr):
+    a = arr.shape
+    return a[0], a[1], a[2]
+
+
+def keras_dim_ordering(arr, order='th'):
+    """ Return a given numpy (image) array with Keras dimension ordering.
+        order={'th'=Theano, 'tf'=TensorFlow} where:
+            'th': (samples, channels, width, height)
+            'tf': (samples, width, height, channels)
+    """
+    if order == 'th':
+        return arr.swapaxes(0,2)
+    elif order == 'tf':
+    return arr.swapaxes(0,1)
+    else:
+        raise ValueError("Dim ordering must be order='th' or 'tf'.")
+    
+    
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 
-from imageproc_tk.common import img2arr
+from imageproc_tk.common import *
 import pytest
 
+GH_arr = img2arr("data/GH.jpg") # h=848, w=640, c=3
+
 def test_img2arr():
-    assert img2arr("data/GH.jpg").shape == (848,640,3)
+    assert GH_arr.shape == (848,640,3)
     assert img2arr("data/GH.jpg", scale_width=0.5).shape == (848,320,3)
     assert img2arr("data/GH.jpg", set_width=5, set_height=5).shape == (5,5,3)
 
 #    with pytest.raises(TypeError):
 #        img2arr(...)
+
+def test_height_width_channels():
+    height, width, channels = height_width_channels(GH_arr)
+    assert height == 848
+    assert width == 640
+    assert channels == 3
+    
+
+def test_keras_dim_ordering():
+    assert keras_dim_ordering(GH_arr, order='th').shape == (3,640,848)
+    assert keras_dim_ordering(GH_arr, order='tf').shape == (640,848,3)
+    with pytest.raises(ValueError):
+        keras_dim_ordering(GH_arr, order='something terribly misguided')
